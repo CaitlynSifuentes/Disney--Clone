b16419730b4165afeb2e932de10ed222b90ae3e9
Caitlyn Sifuentes <45127570+CaitlynSifuentes@users.noreply.github.com>
2022-02-22 21:07:08 -0600

    Creating Header Component

---
+++ src/components/Home.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+function Home() {
+  return (
+    <Container>
+        
+    </Container>
+  )
+}
+
+export default Home
+
+// Styled Components
+const Container = styled.div`
+
+`
